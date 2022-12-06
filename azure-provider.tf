64b06666e55f7457f11c74e1843be292a2359f89
gopal <gopal1409@gmail.com>
2022-12-06 07:43:35 +0530

    terraform resource group

--- azure-provider.tf
+++ azure-provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-        #terraform init. it will download the exe file terraform api required to communicate with the cloud service provider
+      #terraform init. it will download the exe file terraform api required to communicate with the cloud service provider
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
       #this is the latest version of terraform provider block
