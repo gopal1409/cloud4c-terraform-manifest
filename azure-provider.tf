376c5c87ee13088568ee641e9d3c8725adbd9509
gopal <gopal1409@gmail.com>
2022-12-06 07:17:16 +0530

    terraform provider

---
+++ azure-provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+        #terraform init. it will download the exe file terraform api required to communicate with the cloud service provider
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+      #this is the latest version of terraform provider block
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
